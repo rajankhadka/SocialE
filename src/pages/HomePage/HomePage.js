606c00e3b17d6d3485316fe975783cd681ee5664
Rajan Khadka <rajankhadka5@gmail.com>
2021-03-02 22:11:52 +0545

    creating body content

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -6,8 +6,8 @@ import Header from '../../components/Header/Header';
 import SideBar from '../../components/SideBar/SideBar';
 
 //material UI
-import { IconButton } from '@material-ui/core';
-import { Close, Group, Home, TrackChanges, Warning } from '@material-ui/icons';
+import { Button, TextField } from '@material-ui/core';
+import { Add, Search } from '@material-ui/icons';
 
 
 function HomePage(props) {
@@ -18,7 +18,37 @@ function HomePage(props) {
                 <SideBar />
 
                 <div className={classes.homePage__body__body}>
-                    
+                    <div className={classes.homePage__body__bodyHeader}>
+                        <div className={classes.homePage__body__bodyHeaderTitle}>
+                            <h1>Companies</h1>
+                        </div>
+
+                        <div className={classes.homePage__body__bodyHeaderCompany}>
+                            <div className={classes.homePage__body__bodyHeaderCompanyLeft}>
+                                <Button variant="contained"
+                                    startIcon={<Add />}
+                                    style={{
+                                        fontSize: 15,
+                                        textTransform: 'capitalize',
+                                        fontWeight: 'lighter',
+                                        marginLeft: "10px"
+                                    }}
+                                >
+                                    Add New Company
+                                </Button>
+                            </div>
+
+                            <div className={classes.homePage__body__bodyHeaderCompanyRight}>
+                                <TextField id="outlined-basic"
+                                    variant="outlined"
+                                    placeholder="Search company"
+                                    InputProps={{
+                                        startAdornment: <Search />
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
