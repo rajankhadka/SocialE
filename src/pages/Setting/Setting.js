e855cea54781a757f3b6f2354a1221c843851f89
Rajan Khadka <rajankhadka5@gmail.com>
2021-04-10 14:33:10 +0545

    forget password and enable and disable 2fa

--- src/pages/Setting/Setting.js
+++ src/pages/Setting/Setting.js
@@ -1,5 +1,7 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import classes from "./Setting.module.css";
+import QRCode from "qrcode.react";
+
 
 //material ui
 import { Button, FormControl, FormControlLabel, Radio, RadioGroup, Switch } from '@material-ui/core';
@@ -8,13 +10,156 @@ import { Button, FormControl, FormControlLabel, Radio, RadioGroup, Switch } from
 import Header from '../../components/Header/Header';
 import SideBar from '../../components/SideBar/SideBar';
 
+
 function Setting(props) {
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/status/auth/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${window.localStorage.getItem('token')}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.email_two_factor_auth === true) {
+                    setTwoAuth('email');
+                    setEnableTwoAuth(true);
+                } else if (data.totp_two_factor_auth === true) {
+                    setTwoAuth('qrcode');
+                    setEnableTwoAuth(true);
+                } else if (data.email_and_sms_two_factor_auth === true) {
+                    setTwoAuth("sms");
+                    setEnableTwoAuth(true);
+                } else {
+                    setTwoAuth("email");
+                    setEnableTwoAuth(false)
+                }
+            })
+            .catch(err => console.log(err));
+    }, []);
 
     const [enableTwoAuth, setEnableTwoAuth] = useState(false);
     const [twoAuth, setTwoAuth] = useState("email");
-    
+
+    // const [email_two_factor_auth, setEmail_two_factor_auth] = useState(false);
+    // const [totp_two_factor_auth, setTotp_two_factor_auth] = useState(false);
+    // const [email_and_sms_two_factor_auth, setEmail_and_sms_two_factor_auth] = useState(false);
+
+
+
+    //qrcode img
+    const [qrcodeimg, setQrcodeimg] = useState("");
 
     const [changePassword, setChangePassword] = useState(false);
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errmsg, setErrmsg] = useState("");
+    const [successmsg, setSuccessmsg] = useState("");
+
+    const twoauthHandler = () => {
+        let email_two_factor_auth = false;
+        let totp_two_factor_auth = false;
+        let email_and_sms_two_factor_auth = false;
+        console.log("twoauthHandler");
+        console.log(twoAuth)
+        let token = (window.localStorage.getItem('token'));
+        if (twoAuth === 'email') {
+            console.log("email_two_factor_auth");
+            email_two_factor_auth = true;
+            totp_two_factor_auth = false;
+            email_and_sms_two_factor_auth = false;
+            console.log(email_two_factor_auth,email_and_sms_two_factor_auth,totp_two_factor_auth)
+        } else if (twoAuth === 'sms') {
+            console.log("email_and_sms_two_factor_auth");
+            email_two_factor_auth = false;
+            totp_two_factor_auth = false;
+            email_and_sms_two_factor_auth = true;
+            console.log(email_two_factor_auth,email_and_sms_two_factor_auth,totp_two_factor_auth)
+
+        } else {
+            console.log("totp_two_factor_auth");
+            email_two_factor_auth = false;
+            totp_two_factor_auth = true;
+            email_and_sms_two_factor_auth = false;
+            console.log(email_two_factor_auth,email_and_sms_two_factor_auth,totp_two_factor_auth)
+
+        }
+
+        fetch('http://127.0.0.1:8000/select/twofactor/', {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json",
+                "Authorization":`Token ${token}`
+            },
+            body: JSON.stringify({
+                email_two_factor_auth,
+                totp_two_factor_auth,
+                email_and_sms_two_factor_auth
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                
+            })
+            .catch(err => {
+
+                console.log(err);
+            })
+
+    }
+
+    //password handler
+    const changepasswordHandler = () => {
+        if (newPassword === confirmPassword) {
+            fetch("http://127.0.0.1:8000/user/update/password/", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Token ${window.localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({
+                    old_password: oldPassword,
+                    new_password: newPassword
+                })
+            })
+                .then(response => {
+                    
+                    console.log(response.status);
+                    if (response.status === 403) {
+                        setErrmsg("Old Password doesnot match!!!");
+                        setSuccessmsg("");
+                    } else if (response.status === 400) {
+                        setErrmsg("New Password must be 8 character long");
+                        setSuccessmsg("");
+                    } else {
+                        setErrmsg("");
+                        setSuccessmsg("Successfull Password Changed!!!");
+                        setOldPassword("");
+                        setNewPassword("");
+                        setConfirmPassword("");
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    
+                })
+                .catch(err => {
+                    console.log(err);
+                    
+                });
+            console.log("password change")
+        } else {
+            setErrmsg("New Password Does not match!!!")
+            console.log("password mismatch");
+            setSuccessmsg("");
+        }
+    }
 
     return (
         <div className={classes.setting}>
@@ -38,7 +183,23 @@ function Setting(props) {
                                         name="enableTwoAuth"
                                         onChange={(event) => {
                                             setEnableTwoAuth(event.target.checked)
-                                            console.log(enableTwoAuth)
+                                            console.log(event.target.checked)
+                                            if (event.target.checked === false) {
+                                                console.log("disable")
+                                                fetch("http://127.0.0.1:8000/disable/twofactor/", {
+                                                    method: "POST",
+                                                    headers: {
+                                                        "Content-Type": "application/json",
+                                                        "Authorization":`Token ${window.localStorage.getItem('token')}`
+                                                    },
+                                                    body: JSON.stringify({
+                                                        "status":false
+                                                    })
+                                                })
+                                                    .then(response => response.json())
+                                                    .then(data => console.log(data))
+                                                    .catch(err => console.log(err))
+                                            }
                                         }}
                                         color="primary"
                                     />
@@ -61,7 +222,24 @@ function Setting(props) {
                                                 value={twoAuth}
                                                 onChange={(event) => {
                                                     setTwoAuth(event.target.value);
-                                                    console.log(event.target.value)
+                                                    console.log(event.target.value);
+                                                    if (event.target.value === 'qrcode') {
+                                                        console.log("qrcode called");
+                                                        fetch("http://127.0.0.1:8000/enable/totp/", {
+                                                            method: "POST",
+                                                            headers: {
+                                                                "Content-Type": "application/json",
+                                                                "Authorization": `Token ${window.localStorage.getItem('token')}`
+                                                            }
+                                                        })
+                                                            .then(response => response.json())
+                                                            .then(data => {
+                                                                console.log(data.qrcode)
+                                                                
+                                                                setQrcodeimg(data.qrcode)
+                                                            })
+                                                            .catch(err => console.log("err--->",err));
+                                                    }
                                                 } }
                                             >
                                                 <FormControlLabel value="email" control={<Radio />} label="Email Based" />
@@ -76,7 +254,10 @@ function Setting(props) {
                                                     <div style={{marginLeft:"90px"}}>
                                                         <p >Scan the QR code first</p>
                                                         <div className={classes.qrcodeEnable} >
-                                                            <img alt="qrcode" src="https://i.stack.imgur.com/Mspmr.png" />
+                                                            
+                                                            {/* <Image src={ qrcodeimg}/> */}
+                                                            {/* <img src={ qrcodeimg.toString()} alt="qrcode"/> */}
+                                                            <QRCode value={qrcodeimg} style={{width:"180px",height:"180px",marginLeft:"10px",marginTop:"10px"}}/>
                                                         </div>
                                                     </div>
                                                     
@@ -86,7 +267,8 @@ function Setting(props) {
 
 
                                         <Button color="primary"
-                                            className={classes.setting__twoAuth__button}
+                                                className={classes.setting__twoAuth__button}
+                                                onClick={twoauthHandler}
                                         >
                                             Submit
                                         </Button>
@@ -123,26 +305,56 @@ function Setting(props) {
 
                                 ?
                                     <div className={classes.setting__body__password__body}>
-                            
+                                        {errmsg.length > 0 && <p style={{ marginLeft: "70px", marginBottom: "20px", color: "red" }}>{errmsg}</p>}
+                                        {successmsg.length > 0 && <p style={{ marginLeft: "70px", marginBottom: "20px", color: "green" }}>{ successmsg}</p>}
                                         <div className={classes.oldPassword}>
                                             <label htmlFor="old Password">Old Password:</label>
-                                            <input type="password" id="old Password" name="old Password" />
+                                            <input
+                                                type="password"
+                                                id="old Password"
+                                                name="old Password"
+                                                value={oldPassword}
+                                                onChange={(event) =>{
+                                                    setOldPassword(event.target.value);
+                                                    console.log(event.target.value)
+                                                }}
+                                            />
                                         </div>
                                         
                                         <div className={classes.newPassword}>
                                             <label htmlFor="old Password">new Password:</label>
-                                            <input type="password" id="new Password" name="new Password" />
+                                            <input
+                                                type="password"
+                                                id="new Password"
+                                                name="new Password"
+                                                value={newPassword}
+                                                onChange={(event) =>{
+                                                    setNewPassword(event.target.value);
+                                                    console.log(event.target.value)
+                                                }}
+                                            />
                                         </div>
 
                                         <div className={classes.confirmPassword}>
                                             <label htmlFor="confirm Password">confirm Password:</label>
-                                            <input type="password" id="confirm Password" name="confirm Passwprd" />
+                                            <input
+                                                type="password"
+                                                id="confirm Password"
+                                                name="confirm Password"
+                                                value={confirmPassword}
+                                                onChange={(event) =>{
+                                                    setConfirmPassword(event.target.value);
+                                                    console.log(event.target.value)
+                                                }}
+
+                                            />
                                         </div>
 
                                         <Button color="primary"
                                             className={classes.setting__password__button}
+                                            onClick = {changepasswordHandler}
                                         >
-                                            Submit
+                                            Change Password
                                         </Button>
                                     </div>
                                 : null
